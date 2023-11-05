a3b835dbe239c1551da9ff84419535b13bd962ad
Cam Poole <cameron.poole@fdbhealth.com>
2023-11-05 16:00:02 +0000

    initial commit

--- setup/setup.py
+++ setup/setup.py
@@ -3,9 +3,10 @@ import json
 with open('../json/infoDefault.json', 'r') as file:
     json_data = json.load(file)
 
-print(json_data)
+
 json_data['serverName']=input("Enter server name: ")
 json_data['botToken']=input("Enter Bot Token: ")
+json_data["SteamAPIKey"]=input("Enter Steam Web API Key: ")
 
 with open('../json/info.json', 'w') as file:
     json.dump(json_data, file)
